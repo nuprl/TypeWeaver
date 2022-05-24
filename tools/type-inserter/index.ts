5294e3a5fefaae2e22eb2b141deab4eed96ac2e4
Ming-Ho Yee <mh@mhyee.com>
2022-05-24 11:59:33 -0400

    [type-inserter] read and parse CSV into array of type predictions

--- tools/type-inserter/index.ts
+++ tools/type-inserter/index.ts
@@ -1,24 +1,44 @@
-import { accessSync, constants } from "node:fs";
+import { existsSync, readFileSync } from "node:fs";
+import * as path from "path";
+import { parse } from "csv-parse/sync";
 import { printNode, Project, SourceFile, SyntaxKind } from "ts-morph";
-const path = require('node:path');
 
 const jsFilename: string = process.argv[2];
 const jsPath = path.parse(jsFilename);
+const csvFilename: string = path.join(jsPath.dir, jsPath.name + ".csv");
 const tsFilename: string = path.join(jsPath.dir, jsPath.name + ".ts");
 
-try {
-    accessSync(jsFilename, constants.F_OK);
-} catch (err) {
+if (!existsSync(jsFilename)) {
     console.error(jsFilename + ": file does not exist");
     process.exit(1);
+} else if (!existsSync(csvFilename)) {
+    console.error(csvFilename + ": file does not exist");
+    process.exit(1);
 }
+
+// TODO: read in DT CSV file into a data structure, remove unncessary stuff,
+// align identifiers+types and insert types
+const csvContents = readFileSync(csvFilename);
+const types = parse(csvContents, {
+    cast: true,
+    delimiter: "",
+    on_record: function (record, _) {
+        const isIdentifier = record[1] == "Name.Other";
+        let toReturn = {
+            "token": record[0],
+            "identifier": isIdentifier,
+        };
+        if (isIdentifier) { toReturn["type"] = record[2]; }
+        return toReturn;
+    },
+    relax_column_count: true,
+    trim: true
+});
+console.log(types);
 
 const project: Project = new Project();
 const sourceFile: SourceFile = project.addSourceFileAtPath(jsFilename)
 let outputFile = project.createSourceFile(tsFilename, "", { overwrite: true });
-
-// TODO: read in DT CSV file into a data structure, remove unncessary stuff,
-// align identifiers+types and insert types
 
 function traverse(node: SourceFile): void {
     switch (node.getKind()) {
