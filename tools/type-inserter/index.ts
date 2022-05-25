92e40916d8267da2ff81f0b04d2967d2c4b4139d
Ming-Ho Yee <mh@mhyee.com>
2022-05-25 17:34:52 -0400

    [type-inserter] Finish type-inserter, add comments

--- tools/type-inserter/index.ts
+++ tools/type-inserter/index.ts
@@ -1,83 +1,106 @@
 import { existsSync, readFileSync } from "node:fs";
 import * as path from "path";
 import { parse } from "csv-parse/sync";
-import { printNode, Project, SourceFile, SyntaxKind } from "ts-morph";
+import { Project, SourceFile, SyntaxKind } from "ts-morph";
 
-const jsFilename: string = process.argv[2];
-const jsPath = path.parse(jsFilename);
-const csvFilename: string = path.join(jsPath.dir, jsPath.name + ".csv");
-const tsFilename: string = path.join(jsPath.dir, jsPath.name + ".ts");
-
-if (!existsSync(jsFilename)) {
-    console.error(jsFilename + ": file does not exist");
-    process.exit(1);
-} else if (!existsSync(csvFilename)) {
-    console.error(csvFilename + ": file does not exist");
+function printUsageAndExit(error: string): void {
+    console.log(error);
+    console.log("Usage: node index.js <input>.js");
+    console.log("  <input>.js is expected to be a JavaScript file (without type annotations)");
+    console.log("  <input>.csv must also exist, and contains the predicted types for <input>.js");
     process.exit(1);
 }
 
-// TODO: read in DT CSV file into a data structure, remove unncessary stuff,
-// align identifiers+types and insert types
-const csvContents = readFileSync(csvFilename);
-const types = parse(csvContents, {
-    cast: true,
-    delimiter: "",
-    on_record: function (record, _) {
-        const isIdentifier = record[1] == "Name.Other";
-        let toReturn = {
-            "token": record[0],
-            "identifier": isIdentifier,
-        };
-        if (isIdentifier) { toReturn["type"] = record[2]; }
-        return toReturn;
-    },
+const jsFilename: string = process.argv[2];
+const jsPath: path.ParsedPath = path.parse(jsFilename);
+const csvFilename: string = path.join(jsPath.dir, jsPath.name + ".csv");
+
+if (!existsSync(jsFilename)) {
+    printUsageAndExit("file does not exist: " + jsFilename);
+} else if (!existsSync(csvFilename)) {
+    printUsageAndExit("file does not exist: " + csvFilename);
+}
+
+// Read and parse unannotated JavaScript source.
+// Prepare TypeScript output file by copying JS file.
+const project: Project = new Project();
+const sourceFile: SourceFile = project.addSourceFileAtPath(jsFilename);
+let outputFile: SourceFile = sourceFile.copy(jsPath.name + ".ts", { overwrite: true });
+
+// Read and parse CSV file, which contains tokens and the probability distribution of predicted types.
+// DeepTyper uses \x1f, the ASCII control character "Unit Separator," to separate values.
+// The columns are:
+//   token_val, token_type, type1, prob1, type2, prob2, type3, prob3, type4, prob4, type5, prob5
+// Token types that are "Name.*" are identifiers, and only these tokens have predicted types.
+// The types are listed in decreasing probability.
+// We only keep the records that have types, and throw everything away except the top type prediction.
+const csvContents: Buffer = readFileSync(csvFilename);
+const typePredictions: string[] = parse(csvContents, {
+    delimiter: "\x1f",
+    on_record: (record, _) => record[1].startsWith("Name") ? record[2] : null,
     relax_column_count: true,
     trim: true
 });
-console.log(types);
 
-const project: Project = new Project();
-const sourceFile: SourceFile = project.addSourceFileAtPath(jsFilename)
-let outputFile = project.createSourceFile(tsFilename, "", { overwrite: true });
+// Count how many identifier nodes have been seen.
+// This is used to align with and index into the type predictions array.
+let counter: number = 0;
 
+// This function iterates over all Identifiers in the AST and it sets type annotations for:
+//   - function declarations            function f() { ... }
+//   - function parameters              function f(a, b, c) { ... }
+//   - variable declarations            var/let/const x = 42;
+//     that are not for...in or for...of statements
+//                                      for (var x in/of y) stmt
+//     and are not destructuring assignments
+//                                      let [a, b] = c)
+//   - named function expressions       let fun = function f() { ... }
 function traverse(node: SourceFile): void {
-    switch (node.getKind()) {
-        case SyntaxKind.FunctionDeclaration:
-            let funDecl = node.asKindOrThrow(SyntaxKind.FunctionDeclaration);
-            funDecl.setReturnType("any");
-            break;
-        case SyntaxKind.Parameter:
-            let param = node.asKindOrThrow(SyntaxKind.Parameter);
-            param.setType("any");
-            break;
-        case SyntaxKind.VariableDeclaration:
-            // Only annotate declarations if it's a variable statement, not for loops
-            //   VariableStatement -> VariableDeclarationList -> VariableDeclaration
-            // Only annotate identifires, not binding patterns (aka destructuring patterns)
-            //   VariableDeclaration -> Identifier
-            let varDecl = node.asKindOrThrow(SyntaxKind.VariableDeclaration);
-            let grandparent = varDecl.getParentOrThrow().getParentOrThrow();
-            let children = varDecl.getChildren();
-            if (grandparent.getKind() === SyntaxKind.VariableStatement
-                && varDecl.getChildCount() > 0
-                && children[0].getKind() === SyntaxKind.Identifier) {
-                varDecl.setType("any");
-            }
-            break;
-        case SyntaxKind.FunctionExpression:
-            // TODO: this should probably be handled as part of a VariableDeclaration
-            // Either we set the return type here, or we set it on the declaration.
-            // We can only set it here if the function expression has a name.
-            let funExpr = node.asKindOrThrow(SyntaxKind.FunctionExpression);
-            if (funExpr.getName() !== undefined) {
-                funExpr.setReturnType("any");
-            }
-            break;
+    if (node.getKind() === SyntaxKind.Identifier) {
+        let id = node.asKindOrThrow(SyntaxKind.Identifier);
+        let parentNode = id.getParent();
+
+        // Check if the Identifier's parent node is one of the types of nodes we can annotate.
+        switch (parentNode.getKind()) {
+            case SyntaxKind.FunctionDeclaration:
+                let funDecl = parentNode.asKindOrThrow(SyntaxKind.FunctionDeclaration);
+                funDecl.setReturnType(typePredictions[counter]);
+                break;
+            case SyntaxKind.Parameter:
+                let param = parentNode.asKindOrThrow(SyntaxKind.Parameter);
+                param.setType(typePredictions[counter]);
+                break;
+            case SyntaxKind.VariableDeclaration:
+                // Check that this is a variable statement:
+                //   VariableStatement -> VariableDeclarations -> VariableDeclaration
+                // for...in and for...of statements are not VariableStatements.
+                // Also check that the first child is an Identifier:
+                //   VariableDeclaration -> Identifier
+                // This avoids destructuring assignments.
+                let varDecl = parentNode.asKindOrThrow(SyntaxKind.VariableDeclaration);
+                let varStmt = varDecl.getParentOrThrow().getParentOrThrow();
+                let varDeclChildren = varDecl.getChildren();
+                if (varStmt.getKind() === SyntaxKind.VariableStatement
+                    && varDeclChildren.length > 0
+                    && varDeclChildren[0] === id) {
+                    varDecl.setType(typePredictions[counter]);
+                }
+                break;
+            case SyntaxKind.FunctionExpression:
+                // If the parent node of an Identifier is a FunctionExpression,
+                // then the FunctionExpression is named and has a type prediction, so we set its type.
+                let funExpr = parentNode.asKindOrThrow(SyntaxKind.FunctionExpression);
+                funExpr.setReturnType(typePredictions[counter]);
+                break;
+        }
+        counter++;
     }
     node.forEachChild(traverse);
 }
-traverse(sourceFile);
+traverse(outputFile);
 
-// Write to output file
-//outputFile.insertText(0, sourceFile.getFullText()).saveSync();
-console.log(sourceFile.getFullText());
+// Print annotated code to console
+console.log(outputFile.getFullText());
+
+// Save the modified file
+outputFile.saveSync();
