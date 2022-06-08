6d9fbc648b5222c2a5c48ef70359e55718c594b9
Ming-Ho Yee <mh@mhyee.com>
2022-06-08 16:40:52 -0400

    [wip] More hacking away at type-inserter
    This version "mostly" works to insert types properly. It does not handle
    arrow functions `(a, b) => a + b`, and the algorithm needs to be
    modified to support that.
    
    However, it works for variable declarations, function declarations
    (parameters and return types), and function expressions (parameters,
    return types if the expression is named).
    
    The code is also very messy and repetitive.

--- tools/type-inserter/index.ts
+++ tools/type-inserter/index.ts
@@ -32,25 +32,116 @@ const project: Project = new Project();
 const sourceFile: SourceFile = project.addSourceFileAtPath(jsFilename);
 let outputFile: SourceFile = sourceFile.copy(jsPath.name + ".ts", { overwrite: true });
 
-// Read and parse CSV file, which contains tokens and the probability distribution of predicted types.
-// DeepTyper uses \x1f, the ASCII control character "Unit Separator," to separate values.
-// The columns are:
-//   token_val, token_type, type1, prob1, type2, prob2, type3, prob3, type4, prob4, type5, prob5
-// Token types that are "Name.*" are identifiers, and only these tokens have predicted types.
-// The types are listed in decreasing probability.
-// We only keep the records that have types, and throw everything away except the top type prediction.
+interface Record {
+    tokenValue: string;
+    tokenType: string;
+    typePrediction: string;
+}
+
+// This class parses the CSV file and maintains an array of type predictions.
+// However, the sequence of predictions does not directly correspond to the declarations
+// encountered in an AST traversal of the program. Therefore, the tree traversal
+// will query this class and ask for the predictions for the given identifiers.
+class TypePredictions {
+    readonly records: Record[];
+
+    // Internal index into the records array, used for searching.
+    private index: number;
+
+    // Read and parse CSV file, which contains tokens and the probability distribution of
+    // predicted types.
+    constructor(filename: string) {
+        const contents: Buffer = readFileSync(filename);
+        this.records = parse(contents, {
+            // DeepTyper uses \x1f, the ASCII control character "Unit Separator"
+            delimiter: "\x1f",
+            // The columns are:
+            //   token_val, token_type, type1, prob1, ...
+            on_record: function (record, _) {
+                const tokVal = record[0];
+                const tokType = record[1];
+                const pred = record[2];
+                // Only take tokens that are names or reserved keywords
+                // (since they might be identifiers)
+                if (tokType.startsWith("Name") || tokType.startsWith("Keyword") || tokVal === "=>") {
+                    return {
+                        tokenValue: tokVal,
+                        tokenType: tokType,
+                        typePrediction: pred,
+                    }
+                } else {
+                    return null;
+                }
+            },
+            quote: false,
+            relax_column_count: true,
+            trim: true
+        });
+    }
+}
+
 const csvContents: Buffer = readFileSync(csvFilename);
-const typePredictions: string[] = parse(csvContents, {
+const csvRecords = parse(csvContents, {
     delimiter: "\x1f",
-    on_record: (record, _) => record[1].startsWith("Name") ? record[2] : null,
+    on_record: function (record, _) {
+        const tokVal = record[0];
+        const tokType = record[1];
+        if (tokType.startsWith("Name") || tokType.startsWith("Keyword") || tokVal === "=>") {
+            return record;
+        } else {
+            return null;
+        }
+    },
     quote: false,
     relax_column_count: true,
     trim: true
 });
+let csvLength: number = csvRecords.length;
+let csvIdx: number = 1;
 
-// Count how many identifier nodes have been seen.
-// This is used to align with and index into the type predictions array.
-let counter: number = 0;
+// TODO: be careful about keeping indices within bounds...
+function advanceIdx(idx: number, keyword: string, identifier: string): number {
+    for (let i = idx; i < csvRecords.length - 1; ++i) {
+        const prevRecord = csvRecords[i - 1];
+        const record = csvRecords[i];
+        const nextRecord = csvRecords[i + 1];
+        if (keyword && identifier) {
+            if (record[1] === "Keyword.Declaration" && record[0] === keyword && nextRecord[0] === identifier) {
+                // Because we filtered out comments and whitespace, we can check if
+                // the preceding token is a "for". If so, this declaration is part of
+                // a for loop and we should skip it.
+                if (prevRecord[1] === "Keyword" && prevRecord[0] === "for") {
+                    continue;
+                }
+                return i + 1;
+            }
+        } else if (identifier) {
+            if (nextRecord[0] === identifier) {
+                return i + 1;
+            }
+        }
+    }
+    // TODO: this means we have an error
+    return idx;
+}
+
+// TODO: This is basically string search
+
+
+for (var r of csvRecords) {
+    console.log(r);
+}
+/*
+function debugTraverse(node: SourceFile): void {
+    if (node.getChildCount() == 0) {
+        console.log("  " + node.getKindName() + " " + node.getText().split("\n")[0]);
+    } else {
+        console.log(node.getKindName() + " " + node.getText().split("\n")[0]);
+    }
+    node.forEachChild(debugTraverse);
+}
+debugTraverse(sourceFile);
+*/
 
 // This function iterates over all Identifiers in the AST and it sets type annotations for:
 //   - function declarations            function f() { ... }
@@ -62,51 +153,139 @@ let counter: number = 0;
 //                                      let [a, b] = c)
 //   - named function expressions       let fun = function f() { ... }
 function traverse(node: SourceFile): void {
-    if (node.getKind() === SyntaxKind.Identifier) {
-        let id = node.asKindOrThrow(SyntaxKind.Identifier);
-        let parentNode = id.getParent();
+    switch (node.getKind()) {
+        case SyntaxKind.VariableDeclaration: {
+            const varDecl = node.asKindOrThrow(SyntaxKind.VariableDeclaration);
+            const varDeclChildren = varDecl.getChildren();
+            const varStmt = varDecl
+                .getParentOrThrow()
+                .getParentIfKind(SyntaxKind.VariableStatement);
+            const varDecls = varStmt?.getDeclarations();
+            const varDeclType =
+                (varDecls && varDecls[0] === varDecl)
+                ? varStmt?.getDeclarationKind()
+                : undefined;
+            const identifier = varDecl
+                ?.getChildAtIndexIfKind(0, SyntaxKind.Identifier)
+                ?.getText();
 
-        // Check if the Identifier's parent node is one of the types of nodes we can annotate.
-        switch (parentNode.getKind()) {
-            case SyntaxKind.FunctionDeclaration:
-                let funDecl = parentNode.asKindOrThrow(SyntaxKind.FunctionDeclaration);
-                funDecl.setReturnType(typePredictions[counter]);
-                break;
-            case SyntaxKind.Parameter:
-                let param = parentNode.asKindOrThrow(SyntaxKind.Parameter);
-                param.setType(typePredictions[counter]);
-                break;
-            case SyntaxKind.VariableDeclaration:
-                // Check that this is a variable statement:
-                //   VariableStatement -> VariableDeclarations -> VariableDeclaration
-                // for...in and for...of statements are not VariableStatements.
-                // Also check that the first child is an Identifier:
-                //   VariableDeclaration -> Identifier
-                // This avoids destructuring assignments.
-                let varDecl = parentNode.asKindOrThrow(SyntaxKind.VariableDeclaration);
-                let varStmt = varDecl.getParentOrThrow().getParentOrThrow();
-                let varDeclChildren = varDecl.getChildren();
-                if (varStmt.getKind() === SyntaxKind.VariableStatement
-                    && varDeclChildren.length > 0
-                    && varDeclChildren[0] === id) {
-                    varDecl.setType(typePredictions[counter]);
+            if (varDeclType && varDecl && identifier) {
+                csvIdx = advanceIdx(csvIdx, varDeclType, identifier);
+                const prediction: string = csvRecords[csvIdx][2];
+                if (prediction) {
+                    varDecl.setType(prediction);
+                    //console.log(varDeclType + " " + identifier + ": " + prediction);
+                    //console.log(csvRecords[csvIdx]);
                 }
-                break;
-            case SyntaxKind.FunctionExpression:
-                // If the parent node of an Identifier is a FunctionExpression,
-                // then the FunctionExpression is named and has a type prediction, so we set its type.
-                let funExpr = parentNode.asKindOrThrow(SyntaxKind.FunctionExpression);
-                funExpr.setReturnType(typePredictions[counter]);
-                break;
+            } else if (varStmt && varDecl && identifier) {
+                csvIdx = advanceIdx(csvIdx, undefined, identifier);
+                const prediction: string = csvRecords[csvIdx][2];
+                if (prediction) {
+                    varDecl.setType(prediction);
+                    //console.log(identifier + ": " + prediction);
+                    //console.log(csvRecords[csvIdx]);
+                }
+            }
+            //console.log();
+            break;
         }
-        counter++;
+        case SyntaxKind.FunctionDeclaration: {
+            const funDecl = node.asKindOrThrow(SyntaxKind.FunctionDeclaration);
+            const funName = funDecl.getName();
+            const params = funDecl.getParameters();
+
+            if (funName) {
+                csvIdx = advanceIdx(csvIdx, "function", funName);
+                const prediction: string = csvRecords[csvIdx][2];
+                if (prediction) {
+                    funDecl.setReturnType(prediction);
+                    //console.log("function " + funName + ": " + prediction);
+                    //console.log(csvRecords[csvIdx]);
+                }
+            }
+
+            for (let p of params) {
+                let identifier = p.getName();
+                if (identifier) {
+                    csvIdx = advanceIdx(csvIdx, undefined, identifier);
+                    const prediction: string = csvRecords[csvIdx][2];
+                    if (prediction) {
+                        p.setType(prediction);
+                        //console.log(identifier + ": " + prediction);
+                        //console.log(csvRecords[csvIdx]);
+                    }
+                }
+            }
+            //console.log();
+            break;
+        }
+        case SyntaxKind.FunctionExpression: {
+            const funExpr = node.asKindOrThrow(SyntaxKind.FunctionExpression);
+            const funName = funExpr.getName();
+            const params = funExpr.getParameters();
+
+            if (funName) {
+                csvIdx = advanceIdx(csvIdx, "function", funName);
+                const prediction: string = csvRecords[csvIdx][2];
+                if (prediction) {
+                    funExpr.setReturnType(prediction);
+                    //console.log("function " + funName + ": " + prediction);
+                    //console.log(csvRecords[csvIdx]);
+                }
+                for (let p of params) {
+                    let identifier = p.getName();
+                    if (identifier) {
+                        csvIdx = advanceIdx(csvIdx, undefined, identifier);
+                        const prediction: string = csvRecords[csvIdx][2];
+                        if (prediction) {
+                            p.setType(prediction);
+                            //console.log(identifier + ": " + prediction);
+                            //console.log(csvRecords[csvIdx]);
+                        }
+                    }
+                }
+            } else {
+                const firstParam = params.shift();
+                const firstParamId = firstParam?.getName();
+                if (firstParam) {
+                    csvIdx = advanceIdx(csvIdx, "function", firstParamId);
+                    const prediction: string = csvRecords[csvIdx][2];
+                    if (prediction) {
+                        firstParam.setType(prediction);
+                        //console.log(firstParamId + ": " + prediction);
+                        //console.log(csvRecords[csvIdx]);
+                    }
+                    for (let p of params) {
+                        let identifier = p.getName();
+                        if (identifier) {
+                            csvIdx = advanceIdx(csvIdx, undefined, identifier);
+                            const prediction: string = csvRecords[csvIdx][2];
+                            if (prediction) {
+                                p.setType(prediction);
+                                //console.log(identifier + ": " + prediction);
+                                //console.log(csvRecords[csvIdx]);
+                            }
+                        }
+                    }
+                }
+            }
+            //console.log();
+            break;
+        }
+        case SyntaxKind.ArrowFunction: {
+            // TODO: handle arrow functions
+            // There's no keyword to synchronize with
+            // Probably we'll want a more advanced search that can search for a sequence of tokens, e.g.
+            // (a, b) => ... will search the CSV for the sequence "a", "b", "=>".
+        }
     }
+
     node.forEachChild(traverse);
 }
 traverse(outputFile);
 
 // Print annotated code to console
-console.log(outputFile.getFullText());
+//console.log(outputFile.getFullText());
 
 // Save the modified file
 outputFile.saveSync();
