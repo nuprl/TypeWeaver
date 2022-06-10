c02c373cbec9f3f2411f19be83f12722d768957f
Ming-Ho Yee <mh@mhyee.com>
2022-06-10 17:44:03 -0400

    [type-inserter][wip] some cleanup and documentation

--- tools/type-inserter/index.ts
+++ tools/type-inserter/index.ts
@@ -39,19 +39,22 @@ interface Record {
     typeProbability: number;
 }
 
-// This class parses the CSV file and maintains an array of type predictions.
-// However, the sequence of predictions does not directly correspond to the declarations
-// encountered in an AST traversal of the program. Therefore, the tree traversal
-// will query this class and ask for the predictions for the given identifiers.
+/**
+ * This class parses the CSV file and maintains an array of type predictions.
+ * However, the sequence of predictions does not directly correspond to the declarations
+ * encountered in an AST traversal of the program. Therefore, the tree traversal
+ * will query this class and ask for the predictions for the given identifiers.
+ */
 class TypePredictions {
+    private readonly debug: boolean;
     readonly records: Record[];
-
     // Internal index into the records array, used for searching.
     private index: number = 0;
 
     // Read and parse CSV file, which contains tokens and the probability distribution of
     // predicted types.
-    constructor(filename: string) {
+    constructor(filename: string, debug = false) {
+        this.debug = debug
         const contents: Buffer = readFileSync(filename);
         this.records = parse(contents, {
             // DeepTyper uses \x1f, the ASCII control character "Unit Separator"
@@ -61,8 +64,8 @@ class TypePredictions {
             on_record: function (record, _) {
                 const tokVal = record[0];
                 const tokType = record[1];
-                // Only take tokens that are names or reserved keywords
-                // (since they might be identifiers)
+                // Only take tokens that are names or keywords (since they might be identifiers).
+                // Also keep "=>" tokens, to use for searching arrow functions.
                 if (tokType.startsWith("Name") || tokType.startsWith("Keyword") || tokVal === "=>") {
                     return {
                         tokenValue: tokVal,
@@ -80,10 +83,20 @@ class TypePredictions {
         });
     }
 
+    /**
+     * Searches the CSV records to get type predictions for the given token sequence.
+     * The assumption is that a given token sequence (for a declaration) is reasonably unique;
+     * if not, then the order it appears in the CSV records is the same order encountered
+     * while traversing the AST.
+     *
+     * @param {string[]} tokens The sequence of tokens to search for type predictions.
+     * @return {string[]} Array of types corresponding to tokens.
+     */
     findTypesForTokens(tokens: string[]): string[] {
-        console.log(tokens);
         const n: number = this.records.length;
         const m: number = tokens.length;
+
+        // Start the search from the current index.
         for (let i = this.index; i <= n - m; ++i) {
             let match: boolean = true;
             let j: number = 0;
@@ -95,19 +108,29 @@ class TypePredictions {
                 }
             }
             if (match) {
+                // Check that the previous token is not "for",
+                // but only if there is a previous token (i.e. current token is not the first).
                 if (i > 0 && this.records[i - 1].tokenValue === "for") {
                     continue;
                 }
                 const entries: Record[] = this.records.slice(i, i + m);
                 const predictions: string[] = entries.map(_ => _.typePrediction);
-                console.log("prediction(s): " + predictions);
-                console.log(entries);
+
+                // Update the index, so that search resumes from this point instead
+                // of starting from the beginning.
                 this.index = i;
+
+                if (this.debug) {
+                    console.log(tokens);
+                    console.log(predictions);
+                    console.log(entries);
+                }
                 return predictions;
             }
         }
-        // TODO: better handling for this?
-        return undefined;
+        // If we reach this point, we couldn't find anything in the CSV file,
+        // which is probably an error.
+        throw "Searching CSV records failed!"
     }
 
     debugPrint(): void {
@@ -123,29 +146,38 @@ class TypePredictions {
 
 const records: TypePredictions = new TypePredictions(csvFilename);
 
-
-/*
-records.debugPrint();
-function debugTraverse(node: SourceFile): void {
-    if (node.getChildCount() == 0) {
-        console.log("  " + node.getKindName() + " " + node.getText().split("\n")[0]);
-    } else {
-        console.log(node.getKindName() + " " + node.getText().split("\n")[0]);
-    }
-    node.forEachChild(debugTraverse);
-}
-debugTraverse(sourceFile);
-*/
-
-// This function iterates over all Identifiers in the AST and it sets type annotations for:
-//   - function declarations            function f() { ... }
-//   - function parameters              function f(a, b, c) { ... }
-//   - variable declarations            var/let/const x = 42;
-//     that are not for...in or for...of statements
-//                                      for (var x in/of y) stmt
-//     and are not destructuring assignments
-//                                      let [a, b] = c)
-//   - named function expressions       let fun = function f() { ... }
+/**
+ * This function iterates the AST, querying the CSV records with a token sequence
+ * to get the predicted types for those tokens. It then sets the type annotations
+ * for those AST nodes.
+ *
+ * Specifically:
+ *   - variable declarations
+ *       e.g. let x = 42 ...
+ *       query: [let, x]
+ *       types: [T1]
+ *       rewriting: let x: T1 = 42 ...
+ *   - function declarations
+ *       e.g. function f(a, b) { ... }
+ *       query: [function, f, a, b]
+ *       types: [undefined, T, S1, S2]
+ *       rewriting: function f(a: S1, b: S2): T { ... }
+ *   - function expressions
+ *       e.g. function(a, b, c) { ... }
+ *       query: [function, a, b, c]
+ *       types: [undefined, T1, T2, T3]
+ *       rewriting: function(a: T1, b: T2, c: T3) { ... }
+ *   - arrow functions
+ *       e.g. (a, b) => a + b
+ *       query: [a, b, =>]
+ *       types: [T1, T2, undefined]
+ *       rewriting: (a: T1, b: T2) => a + b
+ *
+ * Variable declarations must not be in for...in or for...of statements
+ *   e.g. for (var x in/of y) { ... }
+ * and must not be used in destructuring assignments
+ *   e.g. let [a, b] = c
+ */
 function traverse(node: SourceFile): void {
     switch (node.getKind()) {
         case SyntaxKind.VariableDeclaration: {
@@ -172,42 +204,49 @@ function traverse(node: SourceFile): void {
                 // TODO: this can go wrong, hard to align without keyword
                 // e.g. let f = function(x) { let v = x; return v; }, v = 42;
                 // the declaration "v = 42" can take the type from "return v"
+                // For now, we do nothing.
+                /*
                 const [prediction] = records.findTypesForTokens([identifier]);
                 if (prediction) {
                     varDecl.setType(prediction);
                 }
+                */
             }
             break;
         }
         case SyntaxKind.FunctionDeclaration: {
+            // TODO: default parameters
             const funDecl = node.asKindOrThrow(SyntaxKind.FunctionDeclaration);
             const funName = funDecl.getName();
             const params = funDecl.getParameters();
             const paramNames = params.map(_ => _.getName());
 
             const tokens: string[] = ["function", funName].concat(paramNames);
+            // Pattern match and discard the "undefined" prediction for "function."
+            // Separate out the return type prediction for the function name.
             const [_, retType, ...paramTypes]: string[] = records.findTypesForTokens(tokens);
 
-            if (funName && retType) {
+            if (retType) {
                 funDecl.setReturnType(retType);
             }
 
-            for (let i in params) {
-                if (paramNames[i] && paramTypes[i]) {
-                    params[i].setType(paramTypes[i]);
+            params.forEach((p, i) => {
+                if (paramTypes[i]) {
+                    p.setType(paramTypes[i]);
                 }
-            }
+            });
             break;
         }
         case SyntaxKind.FunctionExpression: {
-            // TODO: this isn't as robust, if the function expression is anonymous
-            // we're searching for the token sequence "function param1 param2 ..." which may not be unique
+            // TODO: default parameters
             const funExpr = node.asKindOrThrow(SyntaxKind.FunctionExpression);
             const funName = funExpr.getName();
             const params = funExpr.getParameters();
             const paramNames = params.map(_ => _.getName());
 
+            // funName is optional, so filter it out if it's undefined.
             const tokens: string[] = ["function", funName].concat(paramNames).filter(_ => _);
+            // Pattern match and discard the "undefined" prediction for "function"
             const [_, ...predictions]: string[] = records.findTypesForTokens(tokens);
 
             if (funName) {
@@ -217,29 +256,30 @@ function traverse(node: SourceFile): void {
                 }
             }
 
-            for (let i in params) {
-                if (paramNames[i] && predictions[i]) {
-                    params[i].setType(predictions[i]);
+            params.forEach((p, i) => {
+                if (predictions[i]) {
+                    p.setType(predictions[i]);
                 }
-            }
+            });
             break;
         }
         case SyntaxKind.ArrowFunction: {
-            // TODO: this isn't as robust
-            // we're searching for the token sequence "param1 param2 =>" which may not be unique
+            // TODO: default parameters
             const arrowFun = node.asKindOrThrow(SyntaxKind.ArrowFunction);
             const params = arrowFun.getParameters();
             const paramNames = params.map(_ => _.getName());
 
             const tokens: string[] = paramNames.concat(["=>"])
             const predictions: string[] = records.findTypesForTokens(tokens);
+
+            // Discard the "undefined" prediction for "=>"
             predictions.pop();
 
-            for (let i in params) {
-                if (paramNames[i] && predictions[i]) {
-                    params[i].setType(predictions[i]);
+            params.forEach((p, i) => {
+                if (predictions[i]) {
+                    p.setType(predictions[i]);
                 }
-            }
+            });
             break;
         }
     }
