fb3114e8aef14dd9da04db73d9bdb3dfb58a5bf8
Ming-Ho Yee <mh@mhyee.com>
2022-07-06 16:29:07 -0400

    [tools] clean up runner.py

--- tools/runner.py
+++ tools/runner.py
@@ -2,10 +2,6 @@ from datetime import datetime
 from pathlib import Path
 from subprocess import PIPE
 import argparse, subprocess
-import time
-
-# TODO: cleanup and document
-# maybe have a counter of all files, not just the ones in a package
 
 ANSI_RED = "\033[0;31m"
 ANSI_GREEN = "\033[0;32m"
@@ -16,7 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="JavaScript type inference runner script.")
     parser.add_argument("directory", help="contains JavaScript packages to infer types for")
     group = parser.add_argument_group(title="pipeline step", description="One of the pipeline steps to run. At least one step is required.")
-    group.add_argument("--infer", action="store_true")
+    group.add_argument("--infer", help="Run type inference", action="store_true")
 
     args = parser.parse_args()
     if not args.infer:
@@ -36,7 +32,7 @@ def deeptyper_infer(directory):
     if not deeptyper_path.exists():
         print("Could not find DeepTyper script: {}".format(deeptyper_path))
         exit(1)
-    print("Using DeepTyper script: {}".format(deeptyper_path))
+    print("Inferring types with DeepTyper: {}".format(deeptyper_path))
 
     # Create the out directory, if it doesn't already exist
     out_directory = Path(directory, "..", "DeepTyper-out", "predictions").resolve()
@@ -45,18 +41,27 @@ def deeptyper_infer(directory):
 
     subdirs = sorted([sd.resolve() for sd in directory.iterdir()])
     short_subdirs = [sd.relative_to(directory) for sd in subdirs]
-    num_subdirs = len(subdirs)
-    print("{} packages found".format(num_subdirs))
 
-    for i, (subdir, short_subdir) in enumerate(zip(subdirs, short_subdirs)):
+    num_subdirs = len(subdirs)
+    num_files = len([f.resolve()
+                     for subdir in subdirs
+                     for f in subdir.rglob("*.js") if f.is_file()])
+    print("Found {} files in {} packages".format(num_files, num_subdirs))
+
+    i = 0
+    num_ok = 0
+    num_fail = 0
+    num_skip = 0
+
+    for subdir, short_subdir in zip(subdirs, short_subdirs):
         out_subdir = Path(out_directory, short_subdir).resolve()
 
         files = sorted([f.resolve() for f in subdir.rglob("*.js") if f.is_file()])
         short_files = [f.relative_to(directory) for f in files]
-        num_files = len(files)
 
-        for j, (file, short_file) in enumerate(zip(files, short_files)):
-            print("[{}/{},{}/{}] Inferring types for {}... ".format(i+1, num_subdirs, j+1, num_files, short_file), end="", flush=True)
+        for file, short_file in zip(files, short_files):
+            i += 1
+            print("[{}/{}] {} ... ".format(i, num_files, short_file), end="", flush=True)
 
             csv_file = Path(out_directory, short_file).resolve().with_suffix(".csv")
             err_file = csv_file.with_suffix(".err")
@@ -66,6 +71,7 @@ def deeptyper_infer(directory):
                 input_mtime = file.stat().st_mtime
                 output_mtime = csv_file.stat().st_mtime if csv_file.exists() else err_file.stat().st_mtime
                 if input_mtime < output_mtime:
+                    num_skip += 1
                     print(ANSI_YELLOW + "[SKIP]" + ANSI_RESET, flush=True)
                     continue
 
@@ -79,21 +85,27 @@ def deeptyper_infer(directory):
 
             if result.returncode == 0:
                 csv_output = file.with_suffix(".csv")
-                if not csv_output.exists():
-                    print("Error: expected .csv file to be created on successful run")
-                else:
+                if csv_output.exists():
                     csv_output.rename(csv_file)
+                    num_ok += 1
                     print(ANSI_GREEN + "[ OK ]" + ANSI_RESET, flush=True)
+                else:
+                    print("Error: expected .csv file to be created on successful run")
 
-                # If there are warnings, capture them too
-                if result.stderr:
-                    warn_file = csv_file.with_suffix(".warn")
-                    with open(warn_file, mode="w", encoding="utf-8") as f:
-                        print(result.stderr, file=f)
+                # DeepTyper warnings are unhelpful; every execution generates warnings
+                # if result.stderr:
+                #     warn_file = csv_file.with_suffix(".warn")
+                #     with open(warn_file, mode="w", encoding="utf-8") as f:
+                #         print(result.stderr, file=f)
             else:
                 with open(err_file, mode="w", encoding="utf-8") as f:
                     print(result.stderr, file=f)
+                num_fail += 1
                 print(ANSI_RED + "[FAIL]" + ANSI_RESET, flush=True)
+
+    print("Number of successes: {}".format(num_ok))
+    print("Number of fails: {}".format(num_fail))
+    print("Number of skips: {}".format(num_skip))
 
 def main():
     args = parse_args()
@@ -101,14 +113,14 @@ def main():
     print("Source directory: {}".format(directory))
 
     if args.infer:
-        print("Inferring types with: DeepTyper")
-
         start_time = datetime.now()
         deeptyper_infer(directory)
         end_time = datetime.now()
 
         duration = end_time - start_time
-        print("Finished inferring types")
-        print("Time to infer types: {}".format(duration))
+        total_seconds = duration.seconds
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        print(f"Time to infer types: {hours}:{minutes:02}:{seconds:02}")
 
 main()
