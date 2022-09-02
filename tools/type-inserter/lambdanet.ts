0d847d9a1c14234020406ff26cddae35944d90b1
Ming-Ho Yee <mh@mhyee.com>
2022-09-02 14:49:52 -0400

    [type-inserter] finish handling LambdaNet
    - parse the CSV file, and store it as a lookup table, mapping source
      locations to type predictions
    - pass 1: traverse the AST, looking up type predictions, and storing
      those type predictions in a separate map
    - pass 2: iterate over the map and apply the type predictions
    
    Two passes are needed, since modifying the AST (inserting types) during
    iteration will change all the source locations, affecting the lookup
    process.

--- tools/type-inserter/lambdanet.ts
+++ tools/type-inserter/lambdanet.ts
@@ -1,47 +1,79 @@
 import { parse } from "csv-parse/sync";
 import { readFileSync } from "node:fs";
 import * as path from "path";
-import { Node, Project, SourceFile } from "ts-morph";
-// import { ArrowFunction, FunctionDeclaration, FunctionExpression, Identifier,
-//          Node, ParameterDeclaration, SyntaxKind,
-//          VariableDeclaration, VariableDeclarationKind, VariableStatement } from "ts-morph";
+import { ArrowFunction, FunctionDeclaration, FunctionExpression, Identifier,
+         Node, ParameterDeclaration, Project, SourceFile, SyntaxKind, VariableDeclaration,
+         VariableStatement } from "ts-morph";
 
-interface Record {
-    startRow: number;
+interface CSVRecord {
+    startLine: number;
     startCol: number;
-    endRow: number;
+    endLine: number;
     endCol: number;
     typePrediction: string;
 }
 
+/**
+ * This class parses the CSV file into an array. Then, to make lookup easier,
+ * the array is converted into a map from source locations (encoded as the
+ * string `{startLine}-{startCol}`) to type predictions (strings).
+ */
 class TypePredictions {
     private readonly debug: boolean;
-    private readonly records: Record[];
+    private readonly lookupTable: Record<string,string>;
 
+    // Read and parse CSV file, which contains tokens and the probability distribution of
+    // predicted types.
     constructor(filename: string, debug: boolean = false) {
         this.debug = debug
         const contents: Buffer = readFileSync(filename);
-        this.records = parse(contents, {
+        const records: CSVRecord[] = parse(contents, {
             // The columns are:
-            //   start_row, start_col, end_row, end_col, type1, prob1, ...
-            columns: ["startRow", "startCol", "endRow", "endCol", "typePrediction"],
+            //   start_line, start_pos, end_line, end_pos, type1, prob1, ...
+            columns: ["startLine", "startCol", "endLine", "endCol", "typePrediction"],
             quote: false,
             relax_column_count_more: true,
             trim: true
         });
+
+        // Convert array of predictions to a lookup table
+        let lookupTable: Record<string,string> = {}
+        for (const r of records) {
+            const key: string = `${r.startLine}-${r.startCol}`;
+            const val: string = r.typePrediction;
+            lookupTable[key] = val;
+        }
+        this.lookupTable = lookupTable;
 
         if (this.debug) {
             this.debugPrint();
         }
     }
 
-    // TODO: some kind of lookup method, maybe convert records array into a dictionary
+    /**
+     * Given a source file and AST node, look up and return the node's type prediction.
+     * The source file is needed to get the node's source location information;
+     * furthermore, the source location (given as a character offset into the file)
+     * must be converted into line and column numbers, then encoded as a string for
+     * lookup in the map.
+     *
+     * @param {SourceFile} sourcefile The source file that the AST node is from.
+     * @param {Node} node The AST node to look up a type prediction for.
+     * @return {string | undefined} The type prediction for the given AST node,
+     *  or undefined if no prediction is available.
+     */
+    public lookup(sourceFile: SourceFile, node: Node): string | undefined {
+        const { line, column } = sourceFile.getLineAndColumnAtPos(node.getStart());
+        const key: string = `${line}-${column}`;
+        if (this.debug) {
+            console.log(`${node.getStart()} ${line},${column}: ${node.getText()}`);
+        }
+        return this.lookupTable[key];
+    }
 
     private debugPrint(): void {
-        for (const r of this.records) {
-            const start: string = "(" + r.startRow + "," + r.startCol + ")";
-            const end: string = "(" + r.endRow + "," + r.endCol + ")";
-            console.log(start + "-" + end + ": " + r.typePrediction);
+        for (const key in this.lookupTable) {
+            console.log(`${key} => ${this.lookupTable[key]}`);
         }
     }
 }
@@ -51,8 +83,9 @@ export default class LambdaNet {
     private readonly predictions: TypePredictions;
 
     private readonly project: Project;
-    private readonly sourceFile: SourceFile;
     private outputFile: SourceFile;
+
+    private nodeToPrediction: Map<Node,string> = new Map();
 
     constructor(jsFilename: string, csvFilename: string, debug: boolean = false) {
         this.debug = debug;
@@ -60,25 +93,168 @@ export default class LambdaNet {
         // Read and parse unannotated JavaScript source.
         // Prepare TypeScript output file by copying JS file.
         this.project = new Project();
-        this.sourceFile = this.project.addSourceFileAtPath(jsFilename);
-        this.outputFile = this.sourceFile.copy(path.parse(jsFilename).name + ".ts", { overwrite: true });
+        const sourceFile: SourceFile = this.project.addSourceFileAtPath(jsFilename);
+        this.outputFile = sourceFile.copy(path.parse(jsFilename).name + ".ts", { overwrite: true });
         this.predictions = new TypePredictions(csvFilename, debug);
     }
 
-    // TODO: traverse the AST
+    /**
+    * This function traverses the AST, querying the CSV records with a source location
+    * to get the predicted types for those tokens. However, it cannot set the type
+    * annotation, because that modifies the source locations for every subsequent node.
+    * Instead, we cache the node and its predicted type in a separate map, and run
+    * a second pass to set the types.
+    *
+    * The nodes we can annotate are:
+    *   - variable declarations
+    *   - function declarations
+    *   - function expressions
+    *   - arrow functions
+    *
+    * Variable declarations must not be in for...in or for...of statements
+    *   e.g. for (var x in/of y) { ... }
+    * and must not be used in destructuring assignments
+    *   e.g. let [a, b] = c
+    */
     private traverse = (node: Node): void => {
+        /**
+         * Local function for looking up the type prediction for a node, and then
+         * caching it in a map so the node's type can be set in a second pass.
+         *
+         * Note that for function-like nodes, the node to look up and the node to be
+         * typed are different: the node to look up is the identifier (name) of the
+         * function, while the node to be typed is the actual function node. The
+         * source locations for the two are different. For other kinds of nodes, the
+         * two are the same.
+         *
+         * @param {Node} nodeToLookup The node whose type prediction we want to look up.
+         * @param {Node} nodeToType The node to have its type annotation set; defaults to `nodeToLookup`.
+         */
+        const lookupAndCache = (nodeToLookup: Node, nodeToType: Node = nodeToLookup): void => {
+            const prediction: string | undefined = this.predictions.lookup(this.outputFile, nodeToLookup);
+            if (prediction) {
+                if (this.debug) {
+                    console.log(`\t${nodeToType.getText().replace(/\s+/g, " ").slice(0, 80)}: ${prediction}`);
+                }
+                this.nodeToPrediction.set(nodeToType, prediction);
+            }
+        }
+
+        /**
+        * Local function for handling function nodes, i.e. function expressions
+        * and function declarations. Doesn't handle arrow functions, because those
+        * have no return type annotations.
+        *
+        * Lookup and set the return type.
+        */
+        const handleFunction = (funNode: FunctionDeclaration | FunctionExpression): void => {
+            if (funNode.getNameNode()) {
+                const funId: Identifier = funNode.getNameNodeOrThrow();
+                lookupAndCache(funId, funNode);
+            }
+        };
+
+        /**
+        * Local function for handling function parameters, i.e. parameters of
+        * function expressions, function declarations, and arrow nodes.
+        * Skip if the parameter is a destructuring pattern.
+        *
+        * Otherwise, lookup and set the parameter type.
+        */
+        const handleParams = (funNode: FunctionDeclaration | FunctionExpression | ArrowFunction): void => {
+            for (const p of funNode.getParameters()) {
+                if (!p.getChildAtIndexIfKind(0, SyntaxKind.Identifier)) {
+                    return;
+                }
+                lookupAndCache(p);
+            }
+        };
+
+        switch (node.getKind()) {
+            case SyntaxKind.VariableDeclaration: {
+                /**
+                 * Example: let x = 42
+                 * Result: let x: T = 42
+                 */
+                const varDecl: VariableDeclaration = node.asKindOrThrow(SyntaxKind.VariableDeclaration);
+                const varStmt: VariableStatement | undefined = varDecl
+                    .getParentOrThrow()
+                    .getParentIfKind(SyntaxKind.VariableStatement);
+                const idNode: Identifier | undefined = varDecl.getChildAtIndexIfKind(0, SyntaxKind.Identifier);
+
+                // Skip this case if any of the following are true:
+                //   - grandparent node is not a variable statement (meaning we're in a for loop)
+                //   - first child node is not an identifier (meaning we have a destructuring pattern)
+                if (!varStmt || !idNode) {
+                    break;
+                }
+                lookupAndCache(varDecl);
+                break;
+            }
+            case SyntaxKind.FunctionDeclaration: {
+                /**
+                 * Example: function f(a, b) { ... }
+                 * Results: function f(a: T2, b: T3): T1 { ... }
+                 */
+                const funDecl: FunctionDeclaration = node.asKindOrThrow(SyntaxKind.FunctionDeclaration);
+                handleFunction(funDecl);
+                handleParams(funDecl);
+                break;
+            }
+            case SyntaxKind.FunctionExpression: {
+                /**
+                 * Example: function(a, b, c) { ... }
+                 * Result: function(a: T1, b: T2, c: T3) { ... }
+                 */
+                const funExpr: FunctionExpression = node.asKindOrThrow(SyntaxKind.FunctionExpression);
+                handleFunction(funExpr);
+                handleParams(funExpr);
+                break;
+            }
+            case SyntaxKind.ArrowFunction: {
+                /**
+                 * Example: (a, b) => a + b
+                 * Result: (a: T1, b: T2) => a + b
+                 */
+                const arrowFun: ArrowFunction = node.asKindOrThrow(SyntaxKind.ArrowFunction);
+                handleParams(arrowFun);
+                break;
+            }
+        }
+        node.forEachChild(this.traverse);
     }
 
     public run() {
-        // Run type insertion
+        // Pass 1: traverse the AST and look up type predictions for nodes that can be typed.
+        // This pass also caches the type predictions in a map of nodes to type predictions.
         this.traverse(this.outputFile);
+
+        // Pass 2: apply the type predictions. We need to do a pattern match to cast the
+        // nodes to the right type, in order to set their type annotations. Specifically,
+        // function nodes use the `setReturnType` method, while other nodes use `setType`.
+        for (const [node, prediction] of this.nodeToPrediction) {
+            switch (node.getKind()) {
+                case SyntaxKind.FunctionDeclaration:
+                    node.asKindOrThrow(SyntaxKind.FunctionDeclaration).setReturnType(prediction);
+                    break;
+                case SyntaxKind.FunctionExpression:
+                    node.asKindOrThrow(SyntaxKind.FunctionExpression).setReturnType(prediction);
+                    break;
+                case SyntaxKind.Parameter:
+                    node.asKindOrThrow(SyntaxKind.Parameter).setType(prediction);
+                    break;
+                case SyntaxKind.VariableDeclaration:
+                    node.asKindOrThrow(SyntaxKind.VariableDeclaration).setType(prediction);
+                    break;
+            }
+        }
 
         if (this.debug) {
             // Print annotated code to console
-            //console.log(this.outputFile.getFullText());
+            console.log(this.outputFile.getFullText());
         }
 
         // Save the modified file
-        //this.outputFile.saveSync();
+        this.outputFile.saveSync();
     }
 }
