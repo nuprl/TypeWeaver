1a72a098fa8eaae88b5eb3ea8295784698abc746
Ming-Ho Yee <mh@mhyee.com>
2022-12-06 11:30:47 -0500

    [DeepTyper] don't need to copy entire src directory

--- DeepTyper/run.sh
+++ DeepTyper/run.sh
@@ -2,6 +2,7 @@
 
 DOCKER=podman
 
+# need --security-opt and --hooks to enable GPU access
 $DOCKER run --rm \
     --security-opt=label=disable \
     --hooks-dir=/usr/share/containers/oci/hooks.d/ \
