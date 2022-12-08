39ce9a11a00968e41c49b697fc91df17b26f871e
Ming-Ho Yee <mh@mhyee.com>
2022-12-08 00:17:50 -0500

    Update dataset: remove .gitignore, add ignored files

---
+++ data/full/original/top1k-typed-nodeps-es6/async/support/build/compile-module.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env nodeimport yargs from 'yargs';
+import fs from 'fs';
+import { transformFile } from 'babel-core';
+import pluginCJS from 'babel-plugin-transform-es2015-modules-commonjs';
+import pluginModuleExports from 'babel-plugin-add-module-exports';
+
+compileModule(yargs.argv, (err) => {
+    if (err) throw err;
+})
+
+function compileModule(options, callback) {
+    const {file, output} = options;
+    const plugins = [
+        pluginModuleExports,
+        pluginCJS
+    ];
+
+    transformFile(file, {
+        babelrc: false,
+        ast: false,
+        plugins
+    }, (err, content) => {
+        if (err) return callback(err);
+        if (!output) {
+            process.stdout.write(content.code);
+            return callback();
+        }
+        fs.writeFile(output, content.code, callback)
+    })
+}
